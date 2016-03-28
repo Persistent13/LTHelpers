bfa1dcfa9fc5d47bea2c0db43cc62ba3a4014eed
Dakota Clark <dakota@allclarks.com>
2016-03-28 15:59:02 -0700

    Windows update; CBT module; etc.

---
+++ DaysSinceUpdate.ps1
@@ -0,0 +1,12 @@
+﻿# Connect to Windows Update
+$wuSession = New-Object -ComObject Microsoft.Update.Session
+$searchSession = $wuSession.CreateUpdateSearcher()
+
+# Save Windows Update status of last update
+$wuStatus = $searchSession.QueryHistory(1,1)
+
+# Gets days since last updates where applied
+$daysSinceLastUpdate = $($wuStatus).Date - $(Get-Date)
+
+# We remove the "-" by pulling the absolute value
+[Math]::Abs($daysSinceLastUpdate.Days)
---
+++ LTPopup/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LTHelpers
+{
+    class PatchingPopup
+    {
+        public static void Main()
+        {
+            System.Windows.Forms.MessageBox.Show("My voice is my passport, verify me.");
+        }
+    }
+}
