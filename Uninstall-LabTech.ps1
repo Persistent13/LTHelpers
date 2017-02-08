219c22a9f7d3bfee13348ec821a70d52db144184
Dakota Clark <dakota@allclarks.com>
2017-02-08 15:48:17 -0800

    I dunno.

---
+++ Uninstall-LabTech.ps1
@@ -0,0 +1,4 @@
+﻿$lt = gwmi win32_product -Filter "Name LIKE 'LabTech C%'"
+$lt.Uninstall() | Out-Null
+rm HKCU:\SOFTWARE\LabTech\Client -Recurse -Force
+rm "$env:ProgramData\LabTech Client" -Recurse -Force
